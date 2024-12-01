2ef7906c465bb49828075f8c57df4f9f22113c7a
Moe <zeussuez2019@gmail.com>
2024-12-01 18:40:44 +0200

    full i think

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,33 +1,46 @@
+import React from "react";
 import type { Metadata } from "next";
-import "../globals.css";
+import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import Topbar from "@/components/shared/Topbar";
+import { dark } from "@clerk/themes";
+
+import "../globals.css";
 import LeftSidebar from "@/components/shared/LeftSidebar";
-import RightSidebar from "@/components/shared/RightSidebar";
 import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Flow",
-  description: "A Nextjs Social Media Application",
+  description: "A Next.js 13 Meta Threads application",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <ClerkProvider >
-      <html lang="en" suppressHydrationWarning>
-        <body className={` antialiased`}>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
+      <html lang='en'>
+        <body className={inter.className}>
           <Topbar />
-          <main>
+
+          <main className='flex flex-row'>
             <LeftSidebar />
-            <section className="main-container">
-              <div className="w-full max-w-4xl">{children}</div>
+            <section className='main-container'>
+              <div className='w-full max-w-4xl'>{children}</div>
             </section>
+            {/* @ts-ignore */}
             <RightSidebar />
           </main>
+
           <Bottombar />
         </body>
       </html>
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,30 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    experimental: {
+      serverActions: {}, // ServerActions should be an object, not just a boolean
+      serverComponentsExternalPackages: ["mongoose"],
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "img.clerk.com",
+        },
+        {
+          protocol: "https",
+          hostname: "images.clerk.dev",
+        },
+        {
+          protocol: "https",
+          hostname: "uploadthing.com",
+        },
+        {
+          protocol: "https",
+          hostname: "placehold.co",
+        },
+      ],
+    },
+  };
+  
+  export default nextConfig;
+  
